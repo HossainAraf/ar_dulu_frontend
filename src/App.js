93bb73400164301e878fdd3bb122cdfb2bce253d
Hossain <arafat.kd99@gmail.com>
2024-05-12 18:45:34 +0100

    Install & configure tailwind css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function App() {
   return (
-   <h1>Hello World</h1>
+    <>
+      <h1 class="text-3xl font-bold underline">"Hello World" </h1>
+      <h5 class="text-2xl">- Asadur Rahman Dulu</h5>
+    </>
   );
 }
 

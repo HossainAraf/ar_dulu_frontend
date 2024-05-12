f8d5d2e11201f932ed73245fa975147d0e5e4340
Hossain <arafat.kd99@gmail.com>
2024-05-12 19:54:29 +0100

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-//IMPORTS
+// IMPORTS
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Nav from './Components/Nav'
+import Nav from './Components/Nav';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
+// COMPONENT
 function App() {
   return (
     <Router>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,15 @@
+//IMPORT
+import React from 'react';
 
+//COMPONENT
+const Home = () => {
+    return ( 
+        <div>
+            <h1>Asadur Rahman Dulu</h1>
+            <p>Welcome to the Home Page</p>
+        </div>
+    );
+};
 
 // EXPORT
 export default Home;

708231fe99c1121bfb8ce1f8a9361c4ee3c4dca7
Hossain <arafat.kd99@gmail.com>
2024-05-12 20:02:56 +0100

    Add Awami-Ligue logo to Navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,4 +1,5 @@
 //IMPORT
+import Logo_Awami from '../Assets/logo_awami.png';
 
 
 const Nav = () => {
@@ -6,7 +7,7 @@ const Nav = () => {
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Logo" />
+        <img src={Logo_Awami} class="h-8" alt="Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Asadur Rahman Dulu</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">

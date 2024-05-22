e0a9da7d7560f4b6e9918b9dffc584ff4b38acd4
Hossain <arafat.kd99@gmail.com>
2024-05-22 10:51:58 +0100

    Add slider logic using useEffect hook

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,19 +1,40 @@
-// Slider.jsx
-import React, { useState } from 'react';
+// IMPORT
+import React, { useState, useEffect } from 'react';
 
-const Slider = ({ images }) => {
-// SET THE STATE TO KEEP TRACK OF THE CURRENT SLIDE
+// SLIDER COMPONENT
+const Slider = ({ images, autoSlider =true, autoSliderInterval = 3000 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  // FUNCTION TO GO TO NEXT SLIDE
+  const nextSlide = () => {
+    setCurrentIndex((currentIndex + 1) % images.length);
+  };
+
+  // FUNCTIOIN TO GO TO PREVIOUS SLIDE
+  const prevSlide = () => {
+    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+  };
+
+  // AUTO SLIDER
+  useEffect(() => {
+    if (autoSlider){
+      const slideInterval = setInterval(nextSlide, autoSliderInterval);
+      return () => clearInterval(slideInterval);
+    }
+  }, [currentIndex, autoSlider, autoSliderInterval]);
+
 
   return (
     <div className="relative w-full overflow-hidden">
-      <div className="flex">
+      <div
+      className="flex transition-transform duration-300"
+      style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
         {images.map((image, index) => (
           <img
             key={index}
             src={image}
             alt={`Slide ${index}`}
-            className="sloder-image"
+            className="slider-image"
           />
         ))}
       </div>
